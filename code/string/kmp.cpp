eeb3c23ebb6cb9e08d185c37955488ad9c786f0c
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-09-28 11:42:35 -0300

    improve kmp

--- code/string/kmp.cpp
+++ code/string/kmp.cpp
@@ -2,50 +2,51 @@
 
 using namespace std;
 
-const int MAXN = 100010;
+typedef vector<int> vi;
 
 // NOTEBOOK
 // KMP algorithm to find all occurrences of a pattern in a string
 // Time complexity: O(n + m), where n is the length of the text and m is the
 // length of the pattern
 
-string T, P;     // T -> text P -> pattern
-int    b[MAXN];  // b -> back table
+// T is text
+// P is pattern
+// B is back table
 
-void kmp_preprocess() {
-    // Call this before calling kmp()
-    int n = T.size();
+vi kmp_preprocess(string P) {
     int m = P.size();
     int i = 0, j = -1;
-    b[0] = -1;
+    vi  back(m + 10);
+    back[0] = -1;
     while (i < m) {
-        while (j >= 0 && P[i] != P[j]) j = b[j];
+        while (j >= 0 && P[i] != P[j]) j = back[j];
         i++;
         j++;
-        b[i] = j;
+        back[i] = j;
     }
+    return back;
 }
 
-void kmp() {
-    int n = T.size();
-    int m = P.size();
+void kmp(string T, string P) {
+    vi  back = kmp_preprocess(P);
+    int n    = T.size();
+    int m    = P.size();
     int i = 0, j = 0;
     while (i < n) {
-        while (j >= 0 && T[i] != P[j]) j = b[j];
+        while (j >= 0 && T[i] != P[j]) j = back[j];
         i++;
         j++;
         if (j == m) {
             printf("P is found at index %d in T\n", i - j);
-            j = b[j];
+            j = back[j];
         }
     }
 }
-// NOTEBOOK
 
 int main() {
-    T = "I DO NOT LIKE SEVENTY SEV BUT SEVENTY SEVENTY SEVEN";
-    P = "SEVENTY SEVEN";
-    kmp_preprocess();
-    kmp();
+    string TEXT    = "I DO NOT LIKE SEVENTY SEV BUT SEVENTY SEVENTY SEVEN";
+    string PATTERN = "SEVENTY SEV";
+    kmp(TEXT, PATTERN);
     return 0;
 }
+// NOTEBOOK
