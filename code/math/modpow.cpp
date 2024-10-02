218bf7d934df8dfd5456a5f17b4b38c6c18d63d0
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-10-02 11:10:40 -0300

    add modular exponentiation

---
+++ code/math/modpow.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+// NOTEBOOK
+// Modular Exponentiation
+// Time complexity
+//         O(log(n))
+
+// (x^n) % m
+ull modpow(ull x, ull n, ull m) {
+    if (n == 0) return 1 % m;
+    ull u = modpow(x, n / 2, m);
+    return ((u % m) * (u % m) * (n % 2 ? x % m : 1)) % m;
+}
+// NOTEBOOK
+
+int main() {
+    cout << modpow(321, 34, 97) << endl;
+    return 0;
+}
+
+// NOTEBOOK
