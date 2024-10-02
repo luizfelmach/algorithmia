ae9920a60b88b445e8d0da416585a33942dbb09b
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-10-02 17:44:08 -0300

    update

--- code/math/modpow.cpp
+++ code/math/modpow.cpp
@@ -5,11 +5,12 @@ using namespace std;
 typedef unsigned long long ull;
 
 // NOTEBOOK
+
 // Modular Exponentiation
 // Time complexity
 //         O(log(n))
 
-// (x^n) % m
+/*\[x^n \mod m\]*/
 ull modpow(ull x, ull n, ull m) {
     if (n == 0) return 1 % m;
     ull u = modpow(x, n / 2, m);
@@ -21,5 +22,3 @@ int main() {
     cout << modpow(321, 34, 97) << endl;
     return 0;
 }
-
-// NOTEBOOK
