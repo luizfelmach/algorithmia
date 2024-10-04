7bf364f21744bcc8e99a3c4cef25cf73f7b4fab6
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-10-04 17:55:51 -0300

    add bin pow

--- code/math/modpow.cpp
+++ code/math/modpow.cpp
@@ -13,9 +13,23 @@ ull modpow(ull x, ull n, ull m) {
     ull u = modpow(x, n / 2, m);
     return ((u % m) * (u % m) * (n % 2 ? x % m : 1)) % m;
 }
+
+// Binary Exponentiation
+ull binpow(ull x, ull n, ull m) {
+    x %= m;
+    ull res = 1;
+    while (n > 0) {
+        if (n & 1) res = (res * x) % m;
+        x = (x * x) % m;
+        n >>= 1;
+    }
+    return res;
+}
+
 // NOTEBOOK
 
 int main() {
     cout << modpow(321, 34, 97) << endl;
+    cout << binpow(321, 34, 97) << endl;
     return 0;
 }
